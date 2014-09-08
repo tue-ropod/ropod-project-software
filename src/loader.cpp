d4d58b305c51af0c141a39be41976da3f967f851
Rein Appeldoorn <reinzor@gmail.com>
2014-09-08 15:15:13 +0000

    Implemented heightmaploader
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/ed_object_models@27247 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/loader.cpp
+++ src/loader.cpp
@@ -2,8 +2,15 @@
 
 #include <tue/filesystem/path.h>
 #include <ros/package.h>
+#include <fstream>
 
 #include <geolib/serialization.h>
+#include <geolib/HeightMap.h>
+#include <geolib/Shape.h>
+
+#include <yaml-cpp/yaml.h>
+
+#include <opencv2/highgui/highgui.hpp>
 
 #include <geolib/Importer.h>
 
@@ -29,6 +36,77 @@ Loader::~Loader()
 
 // ----------------------------------------------------------------------------------------------------
 
+geo::ShapePtr getHeightMapShape(const tue::filesystem::Path& path)
+{
+    geo::ShapePtr shape;
+
+    if (path.exists())
+    {
+        std::ifstream fin(path.string().c_str());
+
+        YAML::Parser parser(fin);
+        YAML::Node doc;
+        parser.GetNextDocument(doc);
+
+        double res, origin_x, origin_y, origin_z, blockheight;
+        std::string mapfname = "";
+
+        try
+        {
+            doc["resolution"] >> res;
+            doc["origin"][0] >> origin_x;
+            doc["origin"][1] >> origin_y;
+            doc["origin"][2] >> origin_z;
+            doc["image"] >> mapfname;
+            doc["blockheight"] >> blockheight;
+
+            tue::filesystem::Path img_path(path.string().substr(0, path.string().size()-14) + mapfname);
+
+            if (img_path.exists())
+            {
+                cv::Mat image = cv::imread(img_path.string(), CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
+
+                std::vector<std::vector<double> > map;
+
+                if (image.data)
+                {
+                    map.resize(image.cols);
+                    for(int x = 0; x < image.cols; ++x) {
+                        map[x].resize(image.rows);
+                        for(int y = 0; y < image.rows; ++y) {
+                            map[x][image.rows - y - 1] = blockheight - (double)image.at<unsigned char>(y, x) / 255 * blockheight;
+                        }
+                    }
+                    geo::HeightMap hmap = geo::HeightMap::fromGrid(map, res);
+                    geo::Mesh mesh = hmap.getMesh().getTransformed(geo::Transform(origin_x, origin_y, origin_z));
+                    shape = geo::ShapePtr(new geo::Shape());
+                    shape->setMesh(mesh);
+                }
+                else
+                {
+                    std::cout << "ed::models::Loader: heightmap.pgm contains invalid data." << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "ed::models::Loader: Image specified in heightmap.yaml file does not exist " << img_path.string() << std::endl;
+            }
+        }
+        catch (YAML::InvalidScalar& e)
+        {
+            std::cout << "ed::models::Loader: Failed to parse heightmap.yaml file " << e.what() << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "ed::models::Loader: heightmap.yaml does not exist" << std::endl;
+    }
+
+    return shape;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
 geo::ShapePtr Loader::loadShape(const std::string& type)
 {
     std::map<std::string, geo::ShapePtr>::iterator it_shape = shape_cache_.find(type);
@@ -39,10 +117,15 @@ geo::ShapePtr Loader::loadShape(const std::string& type)
 
     geo::ShapePtr shape;
 
+    tue::filesystem::Path heightmap_path(object_models_path + "/models/" + type + "/shape/heightmap.yaml");
     tue::filesystem::Path shape_path_geo(object_models_path + "/models/" + type + "/shape/shape.geo");
     tue::filesystem::Path shape_path_3ds(object_models_path + "/models/" + type + "/shape/shape.3ds");
     tue::filesystem::Path shape_path_xml(object_models_path + "/models/" + type + "/shape/shape.xml");
 
+    if (!shape && heightmap_path.exists())
+    {
+        shape = getHeightMapShape(heightmap_path);
+    }
     if (!shape && shape_path_geo.exists())
     {
         geo::serialization::registerDeserializer<geo::Shape>();

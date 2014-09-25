d09079e3981c17df4f2f982fd4cfbe4ce417bd6a
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-25 22:08:55 +0000

    Different way of encoding meshes that is easier for scenejs
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/ed_gui_server@27575 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/gui_server_plugin.cpp
+++ src/gui_server_plugin.cpp
@@ -19,10 +19,6 @@ void entityToMsg(const ed::EntityConstPtr& e, ed_gui_server::EntityInfo& msg)
     msg.id = e->id();
     msg.mesh_revision = e->shapeRevision();
     geo::convert(e->pose(), msg.pose);
-
-//    msg.pose.position.x = 0;
-//    msg.pose.position.y = 0;
-//    msg.pose.position.z = 0;
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -135,26 +131,52 @@ bool GUIServerPlugin::srvQueryMeshes(const ed_gui_server::QueryMeshes::Request& 
                 // Mesh revision
                 mesh_msg.revision = e->shapeRevision();
 
-                // Vertices
                 const std::vector<geo::Vector3>& vertices = e->shape()->getMesh().getPoints();
-                mesh_msg.vertices.resize(vertices.size() * 3);
-                for(unsigned int i = 0; i < vertices.size(); ++i)
-                {
-                    mesh_msg.vertices[i * 3] = vertices[i].x;
-                    mesh_msg.vertices[i * 3 + 1] = vertices[i].y;
-                    mesh_msg.vertices[i * 3 + 2] = vertices[i].z;
-                }
 
                 // Triangles
                 const std::vector<geo::TriangleI>& triangles = e->shape()->getMesh().getTriangleIs();
-                mesh_msg.triangles.resize(triangles.size() * 3);
+                mesh_msg.vertices.resize(triangles.size() * 9);
                 for(unsigned int i = 0; i < triangles.size(); ++i)
                 {
                     const geo::TriangleI& t = triangles[i];
-                    mesh_msg.triangles[i * 3] = t.i1_;
-                    mesh_msg.triangles[i * 3 + 1] = t.i2_;
-                    mesh_msg.triangles[i * 3 + 2] = t.i3_;
+                    const geo::Vector3& v1 = vertices[t.i1_];
+                    const geo::Vector3& v2 = vertices[t.i2_];
+                    const geo::Vector3& v3 = vertices[t.i3_];
+
+                    unsigned int i9 = i * 9;
+
+                    mesh_msg.vertices[i9] = v1.x;
+                    mesh_msg.vertices[i9 + 1] = v1.y;
+                    mesh_msg.vertices[i9 + 2] = v1.z;
+                    mesh_msg.vertices[i9 + 3] = v2.x;
+                    mesh_msg.vertices[i9 + 4] = v2.y;
+                    mesh_msg.vertices[i9 + 5] = v2.z;
+                    mesh_msg.vertices[i9 + 6] = v3.x;
+                    mesh_msg.vertices[i9 + 7] = v3.y;
+                    mesh_msg.vertices[i9 + 8] = v3.z;
                 }
+
+
+//                // Vertices
+//                const std::vector<geo::Vector3>& vertices = e->shape()->getMesh().getPoints();
+//                mesh_msg.vertices.resize(vertices.size() * 3);
+//                for(unsigned int i = 0; i < vertices.size(); ++i)
+//                {
+//                    mesh_msg.vertices[i * 3] = vertices[i].x;
+//                    mesh_msg.vertices[i * 3 + 1] = vertices[i].y;
+//                    mesh_msg.vertices[i * 3 + 2] = vertices[i].z;
+//                }
+
+//                // Triangles
+//                const std::vector<geo::TriangleI>& triangles = e->shape()->getMesh().getTriangleIs();
+//                mesh_msg.triangles.resize(triangles.size() * 3);
+//                for(unsigned int i = 0; i < triangles.size(); ++i)
+//                {
+//                    const geo::TriangleI& t = triangles[i];
+//                    mesh_msg.triangles[i * 3] = t.i1_;
+//                    mesh_msg.triangles[i * 3 + 1] = t.i2_;
+//                    mesh_msg.triangles[i * 3 + 2] = t.i3_;
+//                }
             }
         }
         else

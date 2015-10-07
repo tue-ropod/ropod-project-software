1da6db0fec29cfe505ba1d14b43f50dd388e57b2
Ramon Wijnands <rayman747@hotmail.com>
2015-10-07 14:03:19 +0200

    esformatter gui.js

--- app/scripts/gui.js
+++ app/scripts/gui.js
@@ -3,56 +3,58 @@ var entities = []
 
 var cameraControls;
 
-var COLORS =  [ 0x999999, 0x999966, 0x999933,
-                0x996699, 0x996666, 0x996633,
-                0x993399, 0x993366, 0x993333,
-                0x669999, 0x669966, 0x669933,
-                0x666699, 0x666666, 0x666633,
-                0x663399, 0x663366, 0x663333,
-                0x339999, 0x339966, 0x339933,
-                0x336699, 0x336666, 0x336633,
-                0x333399, 0x333366, 0x333333 ];
+var COLORS = [0x999999, 0x999966, 0x999933,
+              0x996699, 0x996666, 0x996633,
+              0x993399, 0x993366, 0x993333,
+              0x669999, 0x669966, 0x669933,
+              0x666699, 0x666666, 0x666633,
+              0x663399, 0x663366, 0x663333,
+              0x339999, 0x339966, 0x339933,
+              0x336699, 0x336666, 0x336633,
+              0x333399, 0x333366, 0x333333];
 
 // --------
 
 var stringToColor = function(str) {
-    for (var i = 0, hash = 5381; i < str.length; hash = ((hash << 5) + hash) + str.charCodeAt(i++));
+  for (var i = 0, hash = 5381; i < str.length; hash = ((hash << 5) + hash) + str.charCodeAt(i++)) ;
 
-    if (hash < 0)
-        hash = -hash;
+  if (hash < 0)
+    hash = -hash;
 
-    var i = hash % COLORS.length;
-    return COLORS[i];
+  var i = hash % COLORS.length;
+  return COLORS[i];
 }
 
 // --------
 
 var ros_host = "localhost";
 var rosUrl = 'ws://' + ros_host + ':9090';
-ros = new ROSLIB.Ros({ url : rosUrl });
+ros = new ROSLIB.Ros({
+  url: rosUrl
+});
 
 // Construct client for requesting meshes
 var clientED = new ROSLIB.Service({
-    ros : ros,
-    name : 'ed/query',
-    serviceType : 'ed/Query'
+  ros: ros,
+  name: 'ed/query',
+  serviceType: 'ed/Query'
 });
 
 // --------
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var clock = new THREE.Clock();
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // Lights
-scene.add( new THREE.AmbientLight( 0x404040 ) );
-var light = new THREE.DirectionalLight( 0xffffff );
-light.position.set( 3, 12, 8 );
-scene.add( light );
+scene.add(new THREE.AmbientLight(0x404040));
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(3, 12, 8);
+scene.add(light);
 
 camera.position.x = 0;
 camera.position.y = -3;
@@ -76,7 +78,7 @@ camera.up = new THREE.Vector3(0, 0, 1);
 
 // cameraControls.addEventListener( 'change', render );
 
-cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 //cameraControls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
 cameraControls.enableDamping = true;
 cameraControls.dampingFactor = 0.25;
@@ -84,82 +86,84 @@ cameraControls.enableZoom = true;
 cameraControls.rotateSpeed = 0.1;
 
 
-var render = function () {
-    var delta = clock.getDelta();
+var render = function() {
+  var delta = clock.getDelta();
 
-    requestAnimationFrame(render);
+  requestAnimationFrame(render);
 
-    cameraControls.update(delta);
+  cameraControls.update(delta);
 
-    renderer.render(scene, camera);
-    // stats.update();
+  renderer.render(scene, camera);
+// stats.update();
 };
 
 // ---------
 
 // Send Entity Info request
 var req = new ROSLIB.ServiceRequest({
-    // id: selectedEntity.id
+  // id: selectedEntity.id
 });
 
 clientED.callService(req, function(result) {
-    console.time('JSON.parse');
-    var update = JSON.parse(result.human_readable);
-    console.timeEnd('JSON.parse');
+  console.time('JSON.parse');
+  var update = JSON.parse(result.human_readable);
+  console.timeEnd('JSON.parse');
 
-    console.time('generate meshes');
-    for(var i = 0; i < update.entities.length; i++) {
-        var e = update.entities[i];
+  console.time('generate meshes');
+  for (var i = 0; i < update.entities.length; i++) {
+    var e = update.entities[i];
 
-        var obj = entities[e.idx];
+    var obj = entities[e.idx];
 
-        if (!obj) {
-            // Does not yet exist
+    if (!obj) {
+      // Does not yet exist
 
-            if (('mesh' in e) && ('pose' in e)) {
-                var material = new THREE.MeshPhongMaterial( { shading: THREE.SmoothShading } )
-                material.color.setHex(stringToColor(e.id));
+      if (('mesh' in e) && ('pose' in e)) {
+        var material = new THREE.MeshPhongMaterial({
+          shading: THREE.SmoothShading
+        })
+        material.color.setHex(stringToColor(e.id));
 
-                var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+        var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
 
-                obj = new THREE.Mesh( geometry, material );
+        obj = new THREE.Mesh(geometry, material);
 
-                scene.add(obj);
-                entities[e.idx] = obj;
-            } else {
-                continue;
-            }
-        }
-
-        if ('mesh' in e) {
-            var geometry = new THREE.Geometry();
-
-            // Construct mesh
-            for(var j = 0; j < e.mesh.vertices.length; j++) {
-                var v = e.mesh.vertices[j];
-                geometry.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
-            }
-
-            for(var j = 0; j < e.mesh.triangles.length; j++) {
-                var t = e.mesh.triangles[j];
-                geometry.faces.push( new THREE.Face3(t.i1, t.i2, t.i3));
-            }
-
-            // Compute normals
-            geometry.computeFaceNormals();
-            // geometry.computeBoundingSphere();
-
-            obj.geometry = geometry;
-        }
-
-        if ('pose' in e) {
-            obj.position.set(e.pose.x, e.pose.y, e.pose.z);
-            obj.quaternion.set(e.pose.qx, e.pose.qy, e.pose.qz, e.pose.qw);
-        }
+        scene.add(obj);
+        entities[e.idx] = obj;
+      } else {
+        continue;
+      }
     }
-    console.timeEnd('generate meshes');
 
-    render();
+    if ('mesh' in e) {
+      var geometry = new THREE.Geometry();
+
+      // Construct mesh
+      for (var j = 0; j < e.mesh.vertices.length; j++) {
+        var v = e.mesh.vertices[j];
+        geometry.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
+      }
+
+      for (var j = 0; j < e.mesh.triangles.length; j++) {
+        var t = e.mesh.triangles[j];
+        geometry.faces.push(new THREE.Face3(t.i1, t.i2, t.i3));
+      }
+
+      // Compute normals
+      geometry.computeFaceNormals();
+      // geometry.computeBoundingSphere();
+
+      obj.geometry = geometry;
+    }
+
+    if ('pose' in e) {
+      obj.position.set(e.pose.x, e.pose.y, e.pose.z);
+      obj.quaternion.set(e.pose.qx, e.pose.qy, e.pose.qz, e.pose.qw);
+    }
+  }
+  console.timeEnd('generate meshes');
+
+  render();
 
 });
 

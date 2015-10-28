32aa44ee072d58124f46868a282a6e294104e35a
Ramon Wijnands <rayman747@hotmail.com>
2015-10-28 15:35:51 +0100

    refactor all THREE.js code into a separate file

--- html/app/scripts/directives/ngwebgl.js
+++ html/app/scripts/directives/ngwebgl.js
@@ -1,252 +1,60 @@
 'use strict';
 
 angular.module('EdGuiApp')
-  .directive('ngWebgl', function () {
+  .directive('ngWebgl', function (robot) {
+    /*globals EdRenderer*/
     return {
-      controller: function ($scope, robot) {
-        var COLORS = [0x999999, 0x999966, 0x999933,
-                        0x996699, 0x996666, 0x996633,
-                        0x993399, 0x993366, 0x993333,
-                        0x669999, 0x669966, 0x669933,
-                        0x666699, 0x666666, 0x666633,
-                        0x663399, 0x663366, 0x663333,
-                        0x339999, 0x339966, 0x339933,
-                        0x336699, 0x336666, 0x336633,
-                        0x333399, 0x333366, 0x333333];
-
-        var stringToColor = function (str) {
-          for (var i = 0, hash = 5381; i < str.length; hash = ((hash << 5) + hash) + str.charCodeAt(i++)) ;
-
-          if (hash < 0)
-            hash = -hash;
-
-          i = hash % COLORS.length;
-          return COLORS[i];
-        }
-
-        function convertVertices(vertices, threeVertices) {
-          for (var i = 0; i < vertices.length; i++) {
-            threeVertices.push(
-              (new THREE.Vector3()).fromArray(vertices[i])
-            );
-          }
-        }
-
-        function convertFaces(faces, threeFaces) {
-          for (var j = 0; j < faces.length; j++) {
-            var face = faces[j];
-            threeFaces.push(new THREE.Face3(face[0], face[1], face[2]));
-          }
-        }
-
-        robot.ed.watch({
-          add: function (obj) {
-            //console.log('add', obj);
-            var geometry = new THREE.Geometry();
-
-            convertVertices(obj.vertices, geometry.vertices);
-            convertFaces(obj.faces, geometry.faces);
-
-            geometry.computeFaceNormals();
-            geometry.computeVertexNormals(true);
-
-            var material = new THREE.MeshLambertMaterial({
-              color: stringToColor(obj.id)
-            });
-
-            var mesh = new THREE.Mesh(geometry, material);
-
-            mesh.position.fromArray(obj.position);
-            mesh.quaternion.fromArray(obj.quaternion);
-
-            $scope.scene.add(mesh);
-            obj.userdata = mesh;
-          },
-          update: function (newObj, oldObj) {
-            var mesh = newObj.userdata = oldObj.userdata;
-            var geometry = mesh.geometry;
-
-            var vupdate = newObj.vertices !== oldObj.vertices;
-            var fupdate = newObj.faces !== oldObj.faces
-
-            if (vupdate) {
-              console.log('update vertices', newObj, oldObj);
-              geometry.vertices = [];
-              convertVertices(newObj.vertices, geometry.vertices);
-            }
-            if (fupdate) {
-              console.log('update faces', newObj, oldObj);
-              geometry.faces = [];
-              convertFaces(newObj.faces, geometry.faces);
-            }
-
-            if (vupdate || fupdate) {
-              geometry.computeFaceNormals(); // modifies .faces
-              geometry.computeVertexNormals(true); // modifies .normals
-
-              geometry.verticesNeedUpdate = true;
-              geometry.elementsNeedUpdate = true;
-              geometry.normalsNeedUpdate = true;
-            }
-          },
-          remove: function (obj) {
-            console.log('remove', obj);
-            var mesh = obj.userdata;
-            $scope.scene.remove(mesh);
-          }
-        });
-      },
+      restrict: 'E',
+      template: '<canvas></canvas>',
       scope: {
         'entitySelection': '&onEntitySelection'
       },
-      link: function postLink(scope, element, attrs) {
+      controllerAs: 'vm',
+      controller: function ($scope) {
+        console.log('scope:', $scope);
+      },
 
-        var cameraControls, raycaster, renderer, light, mouse,
-          contW = element[0].offsetWidth,
-          contH = element[0].offsetHeight;
+      link: function postLink(scope, element) {
+        console.log('linking', scope);
 
-        scope.init = function () {
-
-           // Camera
-          scope.camera = new THREE.PerspectiveCamera(75, contW / contH, 0.1, 1000);
-
-          // For interaction via click
-          raycaster = new THREE.Raycaster();
-          mouse = new THREE.Vector2();
-
-          scope.camera.position.x = 0;
-          scope.camera.position.y = -3;
-          scope.camera.position.z = 3;
-
-          scope.camera.up = new THREE.Vector3(0, 0, 1);
-
-          // Scene
-          scope.scene = new THREE.Scene();
-
-          // Lights
-          scope.scene.add(new THREE.AmbientLight(0x404040));
-          light = new THREE.DirectionalLight(0xffffff);
-          light.position.set(3, 12, 8);
-          scope.scene.add(light);
-
-          function webglAvailable() {
-            try {
-              var canvas = document.createElement( 'canvas' );
-              return !!( window.WebGLRenderingContext && (
-                canvas.getContext( 'webgl' ) ||
-                canvas.getContext( 'experimental-webgl' ) )
-              );
-            } catch ( e ) {
-              return false;
-            }
-          }
-
-          if (webglAvailable()) {
-            renderer = new THREE.WebGLRenderer();
-          } else {
-            renderer = new THREE.CanvasRenderer();
-          }
-
-          renderer = new THREE.WebGLRenderer( { antialias: true } );
-          renderer.setClearColor( 0xf0f0f0 );
-          renderer.setSize( contW, contH );
-
-          cameraControls = new THREE.OrbitControls(scope.camera, renderer.domElement);
-          //cameraControls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
-          cameraControls.enableDamping = true;
-          cameraControls.dampingFactor = 0.25;
-          cameraControls.enableZoom = true;
-          cameraControls.rotateSpeed = 0.1;
-
-          // element is provided by the angular directive
-          element[0].appendChild( renderer.domElement );
-
-          window.addEventListener( 'resize', scope.onWindowResize, false );
-
-        };
-
-        // -----------------------------------
-        // Event listeners
-        // -----------------------------------
-        scope.onWindowResize = function () {
-
-          scope.resizeCanvas();
-
-        };
-
-        function objectSelection(event, object) {
-          var selectedEntity;
-          r.ed.entities.forEach(function(v, k) {
-            if (v.userdata && v.userdata.id == object.id)
-              selectedEntity = v;
-          });
-          scope.entitySelection({entity: selectedEntity, event: event});
-        }
-
-        element.bind('click', function (event, object) {
-
-          event.preventDefault();
-
-          mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
-          mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
-
-          raycaster.setFromCamera( mouse, scope.camera );
-
-          var intersects = raycaster.intersectObjects( scope.scene.children );
-
-          if ( intersects.length > 0 ) {
-            objectSelection(event, intersects[0].object);
-          }
-
+        element.on('$destroy', function () {
+          console.log('ngWebgl: element is destroyed')
         });
 
-        // -----------------------------------
-        // Updates
-        // -----------------------------------
-        scope.resizeCanvas = function () {
+        scope.$on('$destroy', function () {
+          console.log('ngWebgl: scope is destroyed')
+        });
 
-          console.log("resizing canvas");
+        var canvas = element.children();
+        var parent = element.parent();
 
-          contW = element[0].offsetWidth;
-          contH = element[0].offsetHeight;
+        var renderer = new EdRenderer({
+          canvas: canvas.get(0),
+          robot: robot
+        });
 
-          scope.camera.aspect = contW / contH;
-          scope.camera.updateProjectionMatrix();
+        renderer.init();
 
-          renderer.setSize( contW, contH );
+        resizeCanvas();
+        window.addEventListener('resize', resizeCanvas, false);
 
-        };
+        renderer.start();
 
-        // -----------------------------------
-        // Draw and Animate
-        // -----------------------------------
-        scope.animate = function () {
+        element.on('click', function (e) {
+          e.preventDefault();
 
-          requestAnimationFrame( scope.animate );
+          var x = (e.clientX / canvas.innerWidth()) * 2 - 1;
+          var y = - (e.clientY / canvas.innerHeight()) * 2 + 1;
+          var obj = renderer.pickingRay(x, y);
+          console.log('pickingRay(', x, ',', y,'):', obj);
+          if (obj) {
+            scope.entitySelection({entity: obj, event: e});
+          }
+        });
 
-          scope.render();
-
-        };
-
-        scope.render = function () {
-
-          renderer.render( scope.scene, scope.camera );
-
-        };
-
-        // -----------------------------------
-        // Watches scope
-        // -----------------------------------
-
-        // scope.$watch('selectedSceneObjectId', function(newValue, oldValue) {
-        //     
-        //     }
-        // }, true);
-
-        // Begin
-        scope.init();
-        scope.animate();
-
+        function resizeCanvas() {
+          renderer.setSize(parent.innerWidth(), parent.innerHeight());
+        }
       }
     };
   });

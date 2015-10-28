6a3189099b7613541a24e15f4cf956330a15bd08
Ramon Wijnands <rayman747@hotmail.com>
2015-10-28 15:39:30 +0100

    fix eslint errors

--- html/app/scripts/controllers/main.js
+++ html/app/scripts/controllers/main.js
@@ -1,65 +1,62 @@
 'use strict';
 
 angular.module('EdGuiApp')
-	.controller('MainCtrl', function ($scope, robot) {
+  .controller('MainCtrl', function($scope, robot) {
 
-	// Super hacky implementation ;; don't know how to properly interact with these modules, do you know Ramon?
-    $scope.entitySelection = function (action) {
+    // Super hacky implementation ;; don't know how to properly interact with these modules, do you know Ramon?
+    $scope.entitySelection = function(action) {
       var menu = document.getElementById('action-menu');
       menu.style.opacity = 1.0;
 
-      menu.style.left = action.event.offsetX + "px";
-	  menu.style.top = action.event.offsetY + "px";
+      menu.style.left = action.event.offsetX + 'px';
+      menu.style.top = action.event.offsetY + 'px';
 
-	  menu.children[0].innerHTML = action.entity.id;
-
+      menu.children[0].innerHTML = action.entity.id;
     }
 
     // Options for the circular menu
     $scope.options = {
-	  content: 'Menu',
-	  isOpen: false,
-	  toggleOnClick: true,
-	  items: [
-	    {	
-	      content: 'Inspect',
-	      onclick: function () {
-	      	console.log('Inspecting ....');
+      content: 'Menu',
+      isOpen: false,
+      toggleOnClick: true,
+      items: [
+        {
+          content: 'Inspect',
+          onclick: function () {
+            console.log('Inspecting ....');
 
-	      	var elm = document.getElementById('action-menu');
+            var elm = document.getElementById('action-menu');
 
-	      	robot.actionServer.doAction("inspect", elm.children[0].innerHTML);
+            robot.actionServer.doAction('inspect', elm.children[0].innerHTML);
 
-	      	elm.style.opacity = 0;
-	      }
-      	},
-      	{
-	      content: 'Grab',
-	      onclick: function () {
-	      	console.log('Grabbing ....');
+            elm.style.opacity = 0;
+          }
+        }, {
+          content: 'Grab',
+          onclick: function () {
+            console.log('Grabbing ....');
 
-	      	var elm = document.getElementById('action-menu');
+            var elm = document.getElementById('action-menu');
 
-	      	robot.actionServer.doAction("grab", elm.children[0].innerHTML);
+            robot.actionServer.doAction('grab', elm.children[0].innerHTML);
 
-	      	elm.style.opacity = 0;
-	      }
-	    },
-	    {
-	      content: 'Look at',
-	      onclick: function () {
-	      	console.log('Looking at.. ....');
+            elm.style.opacity = 0;
+          }
+        }, {
+          content: 'Look at',
+          onclick: function () {
+            console.log('Looking at.. ....');
 
-	      	var elm = document.getElementById('action-menu');
+            var elm = document.getElementById('action-menu');
 
-	      	robot.actionServer.doAction("look_at", elm.children[0].innerHTML);
+            robot.actionServer.doAction('look_at', elm.children[0].innerHTML);
 
-	      	elm.style.opacity = 0;
-	      }
-	    }
-	  ]
-	};
+            elm.style.opacity = 0;
+          }
+        }
+      ]
+    };
 
-  	console.log("querying ed");
-  	robot.ed.query();
+    console.log('querying ed');
+    robot.ed.query();
   });

a9b279740dc707204cfcaff7a005f8d53d780f2d
Rein Appeldoorn <reinzor@gmail.com>
2015-11-11 15:08:48 +0100

    feat(teleop): Working head and base teleop with use of ngteleopcanvas directive

--- html/app/scripts/controllers/sidebar.js
+++ html/app/scripts/controllers/sidebar.js
@@ -3,14 +3,5 @@
 angular.module('EdGuiApp')
   .controller('SidebarCtrl', function ($scope, $timeout, robot) {
 
-    // query ed with 1 sec interval
-    function updateImage() {
-      $timeout(function () {
-        robot.head.getImage(230, function(rgb_image_url, depth_image_url) {
-          $scope.kinect_image = rgb_image_url;
-          updateImage();
-        });
-      }, 1000)
-    }
-    updateImage();
+    
   });

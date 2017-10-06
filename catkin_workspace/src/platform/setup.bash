35578c86ff652232be4cf30657a224e60d04c806
whoutman <w.houtman@tue.nl>
2017-10-06 10:09:07 +0200

    setup.bash: build->buildit

--- catkin_workspace/src/platform/setup.bash
+++ catkin_workspace/src/platform/setup.bash
@@ -9,7 +9,7 @@ export ED_MODEL_PATH=$CATKIN_WORKSPACE/src/functionalities/ED/ed_object_models/m
 source /opt/ros/kinetic/setup.bash
 source /home/ropod/ropod-project/catkin_workspace/devel/setup.bash
 
-alias build="cd $CATKIN_WORKSPACE; catkin_make"
+alias buildit="cd $CATKIN_WORKSPACE; catkin_make"
 alias clean_build="cd $CATKIN_WORKSPACE;
                    rm -rf build/ devel/;
                    cd $CATKIN_WORKSPACE/src/; 

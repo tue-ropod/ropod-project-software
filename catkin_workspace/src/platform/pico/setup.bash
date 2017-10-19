b3fed093e9b26f01c13aa859d683d5ab2bbffb8c
whoutman <w.houtman@tue.nl>
2017-10-19 11:37:54 +0200

    pico gmapping parameters updated

--- catkin_workspace/src/platform/pico/setup.bash
+++ catkin_workspace/src/platform/pico/setup.bash
@@ -4,7 +4,7 @@ alias pico-wireless="export ROS_MASTER_URI=http://$PICO_IP:11311;
                      export ROS_IP=$PICO_IP;
                      export WIRED_CONNECTION=false;"
 
-echo "pico-wireless"
+pico-wireless
 
 alias pico-wired="export ROS_MASTER_URI=http://$PICO_IP_WIRED:11311;
                   export ROS_IP=$PICO_IP_WIRED;

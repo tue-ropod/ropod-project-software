006fbdbe1cb29087e00d070d7e13bb21438a02c9
cesar <cesar@cesar>
2018-04-23 13:20:19 +0200

    unset ROS_HOSTNME for correct ROS network configuration

--- catkin_workspace/src/platform/ropod/setup.bash
+++ catkin_workspace/src/platform/ropod/setup.bash
@@ -2,10 +2,12 @@
 
 alias ropod-wireless="export ROS_MASTER_URI=http://$ROPOD_IP:11311;
                      export ROS_IP=$ROPOD_IP;
-                     export WIRED_CONNECTION=false;"
+                     export WIRED_CONNECTION=false;
+                     unset ROS_HOSTNAME"
 
 ropod-wireless
 
 alias ropod-wired="export ROS_MASTER_URI=http://$ROPOD_IP_WIRED:11311;
                   export ROS_IP=$ROPOD_IP_WIRED;
-                  export WIRED_CONNECTION=true"
+                  export WIRED_CONNECTION=true;
+                  unset ROS_HOSTNAME"

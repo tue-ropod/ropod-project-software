1a1d7050eaa5510ee006c70cec1352148fb39431
cesar <c.a.lopez.martinez@tue.nl>
2018-09-18 10:20:22 +0200

    Update of parameter names with full namespace - find long term solution later

--- catkin_workspace/src/platform/ropod/ropod_controller/ropod_platform_control_4W/Kinematics_based_control/ropod_4wheel_tdistkinb_cntr/slros_initialize.h
+++ catkin_workspace/src/platform/ropod/ropod_controller/ropod_platform_control_4W/Kinematics_based_control/ropod_4wheel_tdistkinb_cntr/slros_initialize.h
@@ -14,139 +14,139 @@ extern SimulinkSubscriber<geometry_msgs::Twist, SL_Bus_ROPOD_4Wheel_Tdistkinb_cn
 extern SimulinkPublisher<nav_msgs::Odometry, SL_Bus_ROPOD_4Wheel_Tdistkinb_cntr_nav_msgs_Odometry> Pub_ROPOD_4Wheel_Tdistkinb_cntr_1896;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2716;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4249;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter1
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2919;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4250;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter10
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2931;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4251;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter11
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2932;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4252;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter12
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2933;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4253;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter13
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2934;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4254;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter14
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2935;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4255;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter2
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2920;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4256;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter3
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2898;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4257;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter4
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2900;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4258;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter5
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2902;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4259;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter6
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2903;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4260;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter7
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2921;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4261;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter8
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2922;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4262;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Controllers conf/Get Parameter9
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2923;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4263;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2173;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4283;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter1
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2174;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4284;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter10
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2175;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4285;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter11
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2176;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4286;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter12
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2177;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4287;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter13
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2178;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4288;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter14
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2179;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4289;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter15
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2180;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4290;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter16
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2181;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4291;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter17
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2182;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4292;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter2
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2183;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4293;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter3
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2184;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4294;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter4
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2185;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4295;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter5
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2186;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4296;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter6
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2187;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4297;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter7
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2188;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4298;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter8
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2189;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4299;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Ropod kinematic parameters2/Get Parameter9
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2190;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4300;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2213;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4323;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter1
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2214;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4324;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter2
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2215;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4325;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter3
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2216;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4326;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter4
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2217;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4327;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter5
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2218;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4328;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter6
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4048;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4329;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS Smart wheels conf/Get Parameter7
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4050;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4330;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS base conf/Get Parameter
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2237;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4342;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS base conf/Get Parameter1
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2238;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4343;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS base conf/Get Parameter2
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2239;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4344;
 
 // For Block ROPOD_4Wheel_Tdistkinb_cntr/ROS base conf/Get Parameter3
-extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_2240;
+extern SimulinkParameterGetter<real64_T, double> ParamGet_ROPOD_4Wheel_Tdistkinb_cntr_4345;
 
 void slros_node_init(int argc, char** argv);
 
